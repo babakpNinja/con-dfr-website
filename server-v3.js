a6f63c31c045bd148187e711a873840be7983377
CON-DFR Deploy <deploy@con-dfr.org>
2026-01-22 00:22:29 +0000

    Add CORS support for sandbox environment

--- server-v3.js
+++ server-v3.js
@@ -12,6 +12,22 @@ const https = require('https');
 const app = express();
 const PORT = process.env.PORT || 3002;
 
+// CORS middleware for sandbox environment
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // ============================================
 // TRANSLATION SERVICE
 // ============================================
